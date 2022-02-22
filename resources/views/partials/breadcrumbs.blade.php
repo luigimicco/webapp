3373ddcab3a520b1b1951ae824b98d1646b92934
luigimicco <luigi.micco@gmail.com>
2022-02-22 22:26:27 +0100

    add breadcrumbs

---
+++ resources/views/partials/breadcrumbs.blade.php
@@ -0,0 +1,16 @@
+@unless ($breadcrumbs->isEmpty())
+    <div class="d-flex justify-content-between small-box p-1 ">
+      <h1>@yield('title')</h1>
+      <ol class="breadcrumb d-none d-md-inline-flex small">
+          @if (count($breadcrumbs) > 1)
+            @foreach ($breadcrumbs as $breadcrumb)
+                @if (!is_null($breadcrumb->url) && !$loop->last)
+                    <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                @else
+                    <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
+                @endif
+            @endforeach
+          @endif
+      </ol>
+    </div>
+@endunless
---
+++ routes/breadcrumbs.php
@@ -0,0 +1,38 @@
+<?php // routes/breadcrumbs.php
+
+// Note: Laravel will automatically resolve `Breadcrumbs::` without
+// this import. This is nice for IDE syntax and refactoring.
+use Diglactic\Breadcrumbs\Breadcrumbs;
+
+// This import is also not required, and you could replace `BreadcrumbTrail $trail`
+//  with `$trail`. This is nice for IDE type checking and completion.
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+// Home
+Breadcrumbs::for('home', function (BreadcrumbTrail $trail) {
+    $trail->push('Bacheca', route('home'));
+});
+
+// Home > Profilo
+Breadcrumbs::for('profile', function (BreadcrumbTrail $trail) {
+    $trail->parent('home');
+    $trail->push('Profilo', route('profile'));
+});
+
+// Home > Pannello di controllo
+Breadcrumbs::for('admin.admin.acp', function (BreadcrumbTrail $trail) {
+    $trail->parent('home');
+    $trail->push('Pannello di controllo', route('admin.admin.acp'));
+});
+
+// Home > Pannello di controllo > Utenti
+Breadcrumbs::for('admin.users.index', function (BreadcrumbTrail $trail) {
+  $trail->parent('admin.admin.acp');
+  $trail->push('Utenti', route('admin.users.index'));
+});
+
+// Home > Pannello di controllo > Permessi
+Breadcrumbs::for('admin.permissions.index', function (BreadcrumbTrail $trail) {
+  $trail->parent('admin.admin.acp');
+  $trail->push('Permessi', route('admin.permissions.index'));
+});
